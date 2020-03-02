5f165632284e40677adc4057b9cb2d86d87352c6
Giacomo Mc Evoy <giacomo.valenzano@atos.net>
2020-03-02 18:13:11 -0300

    Conditional use of pytest when unit testing

--- scripts/dcluster-test.sh
+++ scripts/dcluster-test.sh
@@ -8,6 +8,13 @@
 # Calculate directory local to script
 BIN_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+# Check availability
+PYTEST2=$($BIN_DIR/find-pytest2.sh)
+TEST_PYTHON3=$(which python3 2> /dev/null)
+if [ $? -eq 0 ]; then
+  WITH_PYTHON3='y'
+fi
+
 cd $BIN_DIR/..
 
 # get a clean source
@@ -21,11 +28,15 @@ MODULE=dcluster
 
 if [ "x$1" == "x--pytest" ]; then
     # with pytest
-    PYTHONPATH=. pytest-2 --pyargs $MODULE -v
-    PYTHONPATH=. pytest-3 --pyargs $MODULE -v
+    PYTHONPATH=. $PYTEST2 --pyargs $MODULE -v
+    if [ x$WITH_PYTHON3 == 'xy' ]; then
+    	PYTHONPATH=. pytest-3 --pyargs $MODULE -v
+    fi
 else
     # with unittest
     PYTHONPATH=. python2 -m unittest discover $MODULE -v
-    PYTHONPATH=. python3 -m unittest discover $MODULE -v
+    if [ x$WITH_PYTHON3 == 'xy' ]; then
+    	PYTHONPATH=. python3 -m unittest discover $MODULE -v
+    fi
 fi
- 
+
---
+++ scripts/find-pytest2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Finds default Python2 pytest
+
+# Giacomo Mc Evoy <giacomo.valenzano@atos.net>
+# Atos 2020
+
+MODERN=$(which pytest-2 2> /dev/null)
+if [ $? -eq 0 ]; then
+  echo $MODERN 
+fi
+
+OLD=$(which py.test 2> /dev/null)
+if [ $? -eq 0 ]; then
+  echo $OLD
+fi
+
+exit 1
+
